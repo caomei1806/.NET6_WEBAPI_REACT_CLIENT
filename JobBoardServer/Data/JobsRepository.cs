ac6bdb3aeeefa105850f037e0df83c4a32688378
Karolina Grochal <46192501+caomei1806@users.noreply.github.com>
2022-02-24 13:41:23 +0100

    CRUD server side

---
+++ JobBoardServer/Data/JobsRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JobBoardServer.Data
+{
+    internal static class JobsRepository
+    {
+        internal async static Task<IEnumerable<Job>> GetJobsAsync()
+        {
+            using (var db = new AppDBContext())
+            {
+                return await db.JobList.ToListAsync();
+            }
+        }
+
+        internal async static Task<Job> GetJobByIdAsync(int jobId)
+        {
+            using(var db = new AppDBContext())
+            {
+                return await db.JobList.FirstOrDefaultAsync(job => job.Id == jobId);
+            }
+        }
+
+        internal async static Task<bool> CreateJobAsync(Job jobToCreate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    await db.JobList.AddAsync(jobToCreate);
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch(Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> UpdateJobAsync(Job jobToUpdate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    db.JobList.Update(jobToUpdate);
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        internal async static Task<bool> DeleteJobAsync(int jobId)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    Job jobToDelete = await GetJobByIdAsync(jobId);
+                    db.Remove(jobToDelete);
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- JobBoardServer/Program.cs
+++ JobBoardServer/Program.cs
@@ -1,20 +1,91 @@
+using JobBoardServer.Data;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1.0", new OpenApiInfo { Title = "ASP.NET React JobBoard", Version = "v1.0" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUIOptions.DocumentTitle = "ASP.NET React JobBoard";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Web API serving Job model");
+    swaggerUIOptions.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
+app.MapGet("/jobs", async () => await JobsRepository.GetJobsAsync())
+    .WithTags("Jobs Endpoints");
+
+app.MapGet("/jobs/{jobId}", async (int jobId) =>
+{
+    Job jobToReturn = await JobsRepository.GetJobByIdAsync(jobId);
+    if(jobToReturn != null)
+    {
+        return Results.Ok(jobToReturn);
+    }
+    else
+    {
+        return Results.BadRequest();
+
+    }
+
+}).WithTags("Jobs Endpoints");
+
+app.MapPost("/jobs-create", async (Job jobToCreate) =>
+{
+    bool createSuccess = await JobsRepository.CreateJobAsync(jobToCreate);
+    if (createSuccess)
+    {
+        return Results.Ok("Create success");
+    }
+    else
+    {
+        return Results.BadRequest();
+
+    }
+
+}).WithTags("Jobs Endpoints");
+
+app.MapPut("/jobs-update", async (Job jobToUpdate) =>
+{
+    bool updateSuccess = await JobsRepository.UpdateJobAsync(jobToUpdate);
+    if (updateSuccess)
+    {
+        return Results.Ok("Update success");
+    }
+    else
+    {
+        return Results.BadRequest();
+
+    }
+
+}).WithTags("Jobs Endpoints");
+
+app.MapDelete("/jobs-delete/{jobId}", async (int jobId) =>
+{
+    bool deleteSuccess = await JobsRepository.DeleteJobAsync(jobId);
+    if (deleteSuccess)
+    {
+        return Results.Ok("Delete success");
+    }
+    else
+    {
+        return Results.BadRequest();
+
+    }
+
+}).WithTags("Jobs Endpoints");
 
 app.Run();
